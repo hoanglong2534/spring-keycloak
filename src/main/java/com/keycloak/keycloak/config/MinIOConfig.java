77a537f42144b5e8977d37de2d1a5abcb6ebe5ec
hoanglong2534 <phamxuanhoanglong@gmail.com>
2026-03-11 16:20:32 +0700

    feat(sys): add minio

---
+++ src/main/java/com/keycloak/keycloak/config/MinIOConfig.java
@@ -0,0 +1,29 @@
+package com.keycloak.keycloak.config;
+
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinIOConfig {
+
+        @Value( "${minio.url}" )
+        private String url;
+
+        @Value( "${minio.access.name}" )
+        private String accessKey;
+
+        @Value( "${minio.access.secret}" )
+        private String accessSecret;
+
+        @Bean
+        public MinioClient minioClient() {
+            return MinioClient.builder()
+                    .endpoint(url)
+                    .credentials(accessKey, accessSecret)
+                    .build();
+        }
+
+}
